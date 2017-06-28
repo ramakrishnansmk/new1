e7a276d97f1c5ec3bdbe47425912e405aca43483
ramakrishnansmk <smkrsulur@gmail.com>
2017-06-28 12:01:15 +0530

    oddeven.C

---
+++ oddeven.C
@@ -0,0 +1,13 @@
+#include<studio.h>
+int main()
+{
+int num;
+{
+printf("enter an integer");
+scanf("%d",&num);
+if(num%2==0)
+printf("%d is even", number);
+else
+printf("%d is odd", number);
+return 0;
+}
