324f1bf4ac3f38d6fb3c993bb5115e6de235705e
ramakrishnansmk <smkrsulur@gmail.com>
2017-06-28 11:40:31 +0530

    positive.C

---
+++ positive.C
@@ -0,0 +1,16 @@
+#include<studio.h>
+int main()
+{
+double num;
+printf("enter a number:");
+scanf("%lf",&num);
+if(num<=0.0)
+{
+printf("you entered 0");
+else
+printf("you entered a negative number");
+}
+else
+printf("you entered a positive number");
+return 0;
+}
